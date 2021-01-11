296ae8f31b09937f980f0d3f2b197be3135c8296
jcastro506 <castrojoshua805.com>
2021-01-11 17:51:38 -0500

    commit tree c0259d207741f4f401be7ddcfb217e28e7ac3273

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe(){
+    function handleButton(event){
+        console.log("Good!")
+    }
+
+    return (
+        <button onClick={handleButton}>Eyes on me</button>
+    )
+}
